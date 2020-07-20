5b467d1d92d9fbccf41735f9b38d652b3dac1539
MickShannahan <beasticorn@gmail.com>
2020-07-20 16:26:38 -0600

    better transition

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -5,6 +5,7 @@ import _store from "../store.js"
 
 function _drawBackground() {
   document.getElementById('load-shade').style.backgroundColor = 'transparent'
+  document.getElementById('load-shade').style.zIndex = `-1`;
   document.body.style.backgroundImage = `url(${_store.State.images.url})`;
   document.body.style.backgroundSize = `100vw, cover`
   console.log('drawbackground ran');
