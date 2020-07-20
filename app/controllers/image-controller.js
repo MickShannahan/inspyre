b5f3aed62adb5f47ef88df58faeebfb896d12a0a
MickShannahan <beasticorn@gmail.com>
2020-07-20 16:36:27 -0600

    back to inspyr

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -7,7 +7,8 @@ function _drawBackground() {
   document.getElementById('load-shade').style.backgroundColor = 'transparent'
   document.getElementById('load-shade').style.zIndex = `-1`;
   document.body.style.backgroundImage = `url(${_store.State.images.url})`;
-  document.body.style.backgroundSize = `100vw, cover`
+  document.body.style.backgroundSize = `cover`
+  document.body.style.height = `100%`
   console.log('drawbackground ran');
 }
 
