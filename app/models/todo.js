474c48d9a0e08ce79ef40660010f9e6f6c270daa
MickShannahan <beasticorn@gmail.com>
2021-01-08 09:37:01 -0700

    fixed ids for new sandbos

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,9 +1,9 @@
-import { generateId } from "../utils.js"
+import { generateId } from "../utils.js";
 
 export default class Todo {
   constructor(data) {
     console.log('[RAW ToDO API DATA]', data);
-    this._id = data._id || generateId()
+    this.id = data._id || generateId()
     this.completed = data.completed || false
     this.user = data.user || 'mick'
     this.description = data.description
